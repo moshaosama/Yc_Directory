a0abd7e272232a6436eac5034de11d6e228bad1a
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 01:53:10 +0300

    Update Navbar.tsx

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
     <>
-      
+      <div className="Container flex justify-between items-center">
+        <div>
+          <Image
+            src={"/Group 5.png"}
+            width={144}
+            height={200}
+            alt="Banner.png"
+          />
+        </div>
+
+        <div className="flex gap-4 items-center">
+          <div>
+            <h1 className="font-bold">Create</h1>
+          </div>
+
+          <div>
+            <h1 className="font-bold text-[#EF4444]">Logout</h1>
+          </div>
+
+          <div>
+            <Image
+              src={"/Frame 70.png"}
+              alt="frame.ong"
+              width={40}
+              height={40}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
