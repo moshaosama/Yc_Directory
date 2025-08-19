a8a4affc6d8d497c488dfec0aaaab463c1b7e721
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 14:49:55 +0300

    Add startup API route for [id]

---
+++ app/api/startup/[id]/route.ts
@@ -0,0 +1,16 @@
+import { Params } from "next/dist/server/request/params";
+import DB from "../../db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const { id } = params;
+  const Query = "SELECT * FROM start_up WHERE id = ?";
+  const Value = [id];
+
+  const [result] = await DB.promise().query(Query, Value);
+
+  return NextResponse.json(result);
+}
