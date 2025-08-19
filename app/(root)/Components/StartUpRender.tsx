52a5d429eeb9186d534c7f4d176120d4c0399f55
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 03:07:11 +0300

    Update StartUpRender.tsx

--- app/(root)/Components/StartUpRender.tsx
+++ app/(root)/Components/StartUpRender.tsx
@@ -1,9 +1,61 @@
-const StartUpRender = () => {
-  return (
-    <>
-      <h1>StartupRender</h1>
-    </>
-  );
-};
+import { startUpService } from "@/Services/StartUp/StartupService";
+import axios from "axios";
+import useGetStartups from "../Hooks/useGetStartups";
+import { Eye } from "lucide-react";
+import Image from "next/image";
 
-export default StartUpRender;
+export default async function StartUpRender() {
+  const StartUps = await useGetStartups();
+
+  return (
+    <div className="Container grid grid-cols-3 gap-10">
+      {Array.isArray(StartUps) &&
+        StartUps?.map((startup) => (
+          <div key={startup.id} className="StartUpCard">
+            <div className="flex justify-between mx-3">
+              <h1>{startup.date}</h1>
+              <div className="flex items-center gap-1">
+                <Eye size={25} color="#EE2B69" />
+                <h1>{startup.view === null ? 0 : startup.view}</h1>
+              </div>
+            </div>
+
+            <div className="mt-10 flex flex-col gap-3">
+              <div className="flex items-center justify-between">
+                <div>
+                  <h1>{startup.name}</h1>
+                  <h1 className="text-xl font-bold">{startup.tag}</h1>
+                </div>
+                <div>
+                  <Image
+                    src={"/Frame 70.png"}
+                    alt="Frame.png"
+                    width={50}
+                    height={50}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-4">
+                <p className="text-[#777777]">{startup.description}</p>
+                <img
+                  src={startup.image_url}
+                  alt="startupImage.png"
+                  width={"100%"}
+                  className="h-60 object-cover"
+                />
+
+                <div className="flex justify-between">
+                  <h1>{startup.tag}</h1>
+
+                  <button className="bg-black text-white font-semibold w-20 py-2 rounded-xl cursor-pointer">
+                    Details
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
