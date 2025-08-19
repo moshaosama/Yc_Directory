b85122d8f40bdfed6ee4a66a486bd71cddd568eb
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 03:07:13 +0300

    Add useGetStartups.tsx hook

---
+++ app/(root)/Hooks/useGetStartups.tsx
@@ -0,0 +1,20 @@
+import { startUpService } from "@/Services/StartUp/StartupService";
+
+interface StartUpData {
+  id: number;
+  date: string;
+  name: string;
+  tag: string;
+  description: string;
+  image_url: string;
+  view: string;
+}
+
+const useGetStartups = async () => {
+  const response = await startUpService.getData();
+  const StartUps: StartUpData[] = (await response).data;
+
+  return StartUps;
+};
+
+export default useGetStartups;
