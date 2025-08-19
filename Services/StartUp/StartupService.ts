87602f9c5ac393cf219cafa4a442dfcffa803ec5
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 14:49:46 +0300

    Update StartupService.ts

--- Services/StartUp/StartupService.ts
+++ Services/StartUp/StartupService.ts
@@ -4,8 +4,21 @@ import { ParentService } from "../parentService";
 class StartUpService extends ParentService {
   createData(data?: any, thunkApi?: any): void {}
   async getData(_?: any, thunkApi?: any) {
-    const response = await axios.get("http://localhost:3000/api/startup");
-    return response;
+    try {
+      const response = await axios.get("http://localhost:3000/api/startup");
+      return response;
+    } catch (err) {
+      return thunkApi?.rejectWithValue(err);
+    }
+  }
+
+  async GetDataByid(id: number, thunkApi?: any) {
+    try {
+      const response = await axios(`http://localhost:3000/api/startup/${id}`);
+      return response;
+    } catch (err) {
+      return thunkApi?.rejectWithValue(err);
+    }
   }
 }
 
