28c893738fd8c2e732d648a3986ce6d32b282d75
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 02:48:44 +0300

    Update FormLogin component

--- app/login/Components/FormLogin.tsx
+++ app/login/Components/FormLogin.tsx
@@ -1,23 +1,43 @@
+"use client"
+
+import {useForm} from "react-hook-form";
+import {authService} from "@/Services/Auth/authService";
+
 export const FormLogin = () => {
+    const {register, handleSubmit} = useForm()
+    const handleLogin = async (data: any) => {
+      await authService.Login(data)
+    }
     return (
-        <>
-            <div className="w-full flex justify-center items-center mt-10">
-                <form className="grid grid--cols-2 gap-6 w-[40pc]">
-                    <p className="ParentForm">
-                        <label className="text-lg font-bold">Email</label>
-                        <input type="text" className="InputForm"/>
-                    </p>
+        <div className="w-full flex justify-center items-center mt-10">
+            <form
+                className="grid grid-cols-2 gap-6 w-[40%]"
+                onSubmit={handleSubmit(handleLogin)}
+            >
+                <p className="ParentForm">
+                    <label className="text-lg font-bold">Email</label>
+                    <input
+                        type="text"
+                        className="InputForm"
+                        {...register("email", { required: true })}
+                    />
+                </p>
 
-                    <p className="ParentForm">
-                        <label className="text-lg font-bold">password</label>
-                        <input type="password" className="InputForm"/>
-                    </p>
+                <p className="ParentForm">
+                    <label className="text-lg font-bold">Password</label>
+                    <input
+                        type="password"
+                        className="InputForm"
+                        {...register("password", { required: true })}
+                    />
+                </p>
 
-                    <p className="col-span-2 w-full flex justify-center">
-                        <button className="BtnLogin">Login</button>
-                    </p>
-                </form>
-            </div>
-        </>
-    )
-}
+                <p className="col-span-2 w-full flex justify-center">
+                    <button type="submit" className="BtnLogin">
+                        Login
+                    </button>
+                </p>
+            </form>
+        </div>
+    );
+};
