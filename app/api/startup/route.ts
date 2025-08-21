2f15b68a84a80308eb32956a9bca051f16ad510c
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 17:37:49 +0300

    Update startup route

--- app/api/startup/route.ts
+++ app/api/startup/route.ts
@@ -3,7 +3,23 @@ import DB from "../db";
 
 export async function GET(request: Request) {
   const Query = "SELECT * FROM start_up";
-  const [result] = await DB.promise().query(Query);
+  const [result] = await DB.query(Query);
 
   return NextResponse.json(result);
+}
+
+
+export async function POST (request: Request) {
+    const body = await request.json();
+    const {title, category,description,image} = body;
+
+    const createStartUpQuery = "INSERT INTO start_up (name, tag, description,image_url) VALUES (?, ?, ?,?)";
+    const Values= [title, category, description, image];
+
+    await DB.query(createStartUpQuery, Values);
+
+    return NextResponse.json({
+        status: 200,
+        message: "Created succesfully"
+    })
 }
