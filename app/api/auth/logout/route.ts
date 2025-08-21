73b6316aeca934651098f3fda4509c7aadeaf914
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 18:51:20 +0300

    Update logout route

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,10 +2,10 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const cookieServer = await cookies();
-  await cookieServer.delete("token");
-
-  return NextResponse.json({
+    const cookieServer = await cookies();
+    await cookieServer.delete("token");
+    await cookieServer.delete("user");
+    return NextResponse.json({
     status: 200,
     message: "Logout Successfully",
   });
