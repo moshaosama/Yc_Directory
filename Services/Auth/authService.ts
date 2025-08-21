0eb8cffbdfe67a0926700290f572b82b67291283
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 17:37:47 +0300

    Update authService.ts

--- Services/Auth/authService.ts
+++ Services/Auth/authService.ts
@@ -1,26 +1,35 @@
 import axios from "axios";
 
 class AuthService {
-    async Login(data: {email: string, password: string}, thunkApi?: any) {
-        try {
-            const response = await axios.post("http://localhost:3002/api/auth/login", data)
-            return response.data;
+  async Login(data: { email: string; password: string }, thunkApi?: any) {
+    try {
+      const response = await axios.post(
+        "http://localhost:3004/api/auth/login",
+        data,
+        {
+          withCredentials: true,
         }
-        catch (err) {
-            return thunkApi?.rejectWithValue(err);
-        }
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi?.rejectWithValue(err);
     }
+  }
 
-    async SignUp (data: {username: string, email :string, password: string}, thunkApi?: any) {
-        try {
-            const response = await axios.post("http://localhost:3002/api/auth/signup", data)
-            return response.data;
-        }
-        catch (err) {
-            return thunkApi?.rejectWithValue(err);
-        }
+  async SignUp(
+    data: { username: string; email: string; password: string },
+    thunkApi?: any
+  ) {
+    try {
+      const response = await axios.post(
+        "http://localhost:3004/api/auth/signup",
+        data
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi?.rejectWithValue(err);
     }
+  }
 }
-
 
 export const authService = new AuthService();
