352e46a7200414292865e49a7107a576bb5987fd
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 18:50:57 +0300

    Add user profile page by user_id

--- app/user/[user_id]/page.tsx
+++ app/user/[user_id]/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import UserCard from "@/app/user/[id]/Components/UserCard";
-import AllStartups from "@/app/user/[id]/Components/AllStartups";
+import UserCard from "@/app/user/[user_id]/Components/UserCard";
+import AllStartups from "@/app/user/[user_id]/Components/AllStartups";
 
-const page = ({ params }: { params: { id: number } }) => {
-    const { id } = params;
+const page = ({ params }: { params: { user_id: number } }) => {
+    const { user_id } = params;
     return (
         <>
             <div className="Container mt-14 flex gap-32">
                 <UserCard />
-                <AllStartups params={{ id: id }} />
+                <AllStartups params={{ user_id: user_id }} />
             </div>
         </>
     );
