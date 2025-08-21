c476fcd1e28c4226ef90c7f03d26377385d465bd
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 18:51:21 +0300

    Update FormCreateStartup component

--- app/startup/create/Components/FormCreateStartup.tsx
+++ app/startup/create/Components/FormCreateStartup.tsx
@@ -1,10 +1,19 @@
+"use client"
+
+import {useForm} from "react-hook-form";
+import {startUpService} from "@/Services/StartUp/StartupService";
 export default function FormCreateStartup () {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const handleCreateStartUp = async (data: any) => {
+        await startUpService.createData(data)
+    }
     return (
         <>
             <div className="flex w-full justify-center mt-10">
                 <form
                     className="grid grid-cols-2 gap-6 w-[40%]"
-
+                    onSubmit={handleSubmit(handleCreateStartUp)}
                 >
                     <p className="ParentForm">
                         <label className="text-lg font-bold">Title</label>
@@ -12,17 +21,20 @@ export default function FormCreateStartup () {
                             type="text"
                             placeholder={"Startup Title"}
                             className="InputForm"
-
+                            {...register("title", {required: true})}
                         />
+
+                        {errors?.title && <p className="text-red-500 text-xl font-bold">Title is required</p>}
                     </p>
 
                     <p className="ParentForm">
                         <label className="text-lg font-bold">Description</label>
                         <textarea
-
                             className="InputForm h-24"
                             placeholder={"Startup Description"}
+                            {...register("description", {required: true})}
                         />
+                        {errors?.description && <p className="text-red-500 text-xl font-bold">Description is required</p>}
                     </p>
 
                     <p className="ParentForm">
@@ -31,7 +43,9 @@ export default function FormCreateStartup () {
                             type="text"
                             className="InputForm"
                             placeholder={"Startup Category (Tech, Education, Health, etc..)"}
+                            {...register("category", {required: true})}
                         />
+                        {errors?.category && <p className="text-red-500 text-xl font-bold">Category is required</p>}
                     </p>
 
                     <p className="ParentForm">
@@ -40,13 +54,16 @@ export default function FormCreateStartup () {
                             type="text"
                             className="InputForm"
                             placeholder={"Startup Image URL"}
+                            {...register("image", {required: true})}
                         />
+
+                        {errors?.image && <p className="text-red-500 text-xl font-bold">Image URL is required</p>}
                     </p>
 
 
                     <p className="col-span-2 w-full flex justify-center">
                         <button type="submit" className="BtnLogin">
-                            Login
+                            Create
                         </button>
                     </p>
                 </form>
