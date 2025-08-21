402640f46ba9051322eefb6cb3d627567a5e33b2
moshaosama <mohamedOSFekry@gmail.com>
2025-08-21 17:37:50 +0300

    Update FormLogin component

--- app/login/Components/FormLogin.tsx
+++ app/login/Components/FormLogin.tsx
@@ -1,43 +1,45 @@
-"use client"
+"use client";
 
-import {useForm} from "react-hook-form";
-import {authService} from "@/Services/Auth/authService";
+import { useForm } from "react-hook-form";
+import { authService } from "@/Services/Auth/authService";
 
 export const FormLogin = () => {
-    const {register, handleSubmit} = useForm()
-    const handleLogin = async (data: any) => {
-      await authService.Login(data)
-    }
-    return (
-        <div className="w-full flex justify-center items-center mt-10">
-            <form
-                className="grid grid-cols-2 gap-6 w-[40%]"
-                onSubmit={handleSubmit(handleLogin)}
-            >
-                <p className="ParentForm">
-                    <label className="text-lg font-bold">Email</label>
-                    <input
-                        type="text"
-                        className="InputForm"
-                        {...register("email", { required: true })}
-                    />
-                </p>
+  const { register, handleSubmit } = useForm();
+  const handleLogin = async (data: any) => {
+    await authService.Login(data);
+    window.location.href = "/";
+  };
 
-                <p className="ParentForm">
-                    <label className="text-lg font-bold">Password</label>
-                    <input
-                        type="password"
-                        className="InputForm"
-                        {...register("password", { required: true })}
-                    />
-                </p>
+  return (
+    <div className="w-full flex justify-center items-center mt-10">
+      <form
+        className="grid grid-cols-2 gap-6 w-[40%]"
+        onSubmit={handleSubmit(handleLogin)}
+      >
+        <p className="ParentForm">
+          <label className="text-lg font-bold">Email</label>
+          <input
+            type="text"
+            className="InputForm"
+            {...register("email", { required: true })}
+          />
+        </p>
 
-                <p className="col-span-2 w-full flex justify-center">
-                    <button type="submit" className="BtnLogin">
-                        Login
-                    </button>
-                </p>
-            </form>
-        </div>
-    );
+        <p className="ParentForm">
+          <label className="text-lg font-bold">Password</label>
+          <input
+            type="password"
+            className="InputForm"
+            {...register("password", { required: true })}
+          />
+        </p>
+
+        <p className="col-span-2 w-full flex justify-center">
+          <button type="submit" className="BtnLogin">
+            Login
+          </button>
+        </p>
+      </form>
+    </div>
+  );
 };
